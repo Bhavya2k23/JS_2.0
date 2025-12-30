27dcb73ca4d1d1893c37a3e5cc087cf405650e04
Bhavya2k23 <myselfprajapati2k04@gmail.com>
2025-12-30 20:53:56 +0530

    Initial commit

---
+++ Basics_One/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 192822
+let accountEmail = "zorobhai2393@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+// accountId = 3 // not allowed
+
+accountEmail = "hagahaga.com"
+accountPassword = "34432"
+accountCity = "Los Santos"
+
+console.log(accountId);
+
+/*
+var use mat karo Beacause issue in block and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ Basics_One/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //Trear all JS code as a newer Version
+
+/*
+alert("Hello") // we are using nodejs, not browser
+
+alert(4 + 5)
+*/
+
+// ECMA Script for Documentation of JS(Specification) and MDN for Documenatation and  
+
+
+let name = "Zoro" //String
+let age = 21 //integer
+let isloggedIn = true //boolean
+
+// number => 2 to power 53
+//bigint 
+// string => ""
+// boolean => true/false
+// null => Standalone value(Representation for empty space)
+// undefined => abhi value assign nhi huyi hai 
+// symbol => to define uniqueness
+
+//object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
