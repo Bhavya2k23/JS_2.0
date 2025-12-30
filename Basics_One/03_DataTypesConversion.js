68926aa460a532df47ad40316fad7268cf960ed5
Bhavya2k23 <myselfprajapati2k04@gmail.com>
2025-12-30 21:12:10 +0530

    Data Type Conversion

---
+++ Basics_One/03_DataTypesConversion.js
@@ -0,0 +1,29 @@
+let score = "Zoro"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// undefined => NaN
+// null => 0
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0=> false
+// "" => false
+// "Zoro" => true
+
+let someNumber = 67
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
